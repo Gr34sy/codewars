fc00b290a47c3da7d11b622fd874325a2a77c361
Gr34sy <slupek3@poczta.fm>
2022-12-28 18:31:49 +0100

    correct task 3 solution

---
+++ 3.Task/script.js
@@ -0,0 +1,4 @@
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+//1
+const filter = (list) => list.filter((el) => typeof el != 'string');
