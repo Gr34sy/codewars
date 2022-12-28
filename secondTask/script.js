4be1a76c05e5ae71c72b2b5d87b603296f726e22
Gr34sy <slupek3@poczta.fm>
2022-12-28 17:47:40 +0100

    probably fixed git user settings on Mac

--- secondTask/script.js
+++ secondTask/script.js
@@ -12,4 +12,3 @@ function validatePIN (pin) {
 // 2 oneliner
 const validatePINo = (pin) => (pin.length===4 || pin.length===6) && [...pin].every(el => '0123456789'.includes(el)) ? true : false;
  
-//sdsd
