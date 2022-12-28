100cf0bca59c0f330fc9d6b8764dabd7ce920be7
Greasy <greasy@Greasys-MacBook-Pro.local>
2022-12-28 17:22:18 +0100

    added task 2 solution

---
+++ secondTask/script.js
@@ -0,0 +1,13 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.If the function is passed a valid PIN string, return true, else return false.
+
+//1 
+function validatePIN (pin) {
+    if((pin.length===4 || pin.length===6) && [...pin].every(el => '0123456789'.includes(el))){
+      return true
+    }else{
+      return false
+    }
+}
+
+//2 oneliner
+// function validatePINo = (pin) =>
