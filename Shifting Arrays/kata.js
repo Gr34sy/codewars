f82c10632e836889837e35740870d4fc9d0dcce6
Gr34sy <slupek3@poczta.fm>
2023-01-13 21:31:38 +0100

    solution

--- Shifting Arrays/kata.js
+++ Shifting Arrays/kata.js
@@ -20,7 +20,7 @@ function addingShifted (arrays, shift) {
     let result = [];
     let start = 0;
     for (let array of arrays) {
-      for (let i = 0; i < array.length; i += 1) {
+      for (let i = 0; i < array.length; i++ ) {
         result[start + i] = (result[start + i] || 0) + array[i];
       }
       start += shift;
