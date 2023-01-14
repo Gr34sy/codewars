36e210d629155b942bdfe4fb913ac33d629558c0
Gr34sy <slupek3@poczta.fm>
2023-01-14 23:34:54 +0100

    started new task

--- Shifting Arrays/kata.js
+++ Shifting Arrays/kata.js
@@ -1,20 +1,4 @@
-// const theShift = (arrays,shift = 0) => {
-//     let agregated = [...arrays[0]];
-   
-//     for(let i=1; i < arrays.length; i++){
-//        console.log('blyb');
-//        for(let j = 0; j < arrays[i].length; j++){
-//         if(agregated[j+shift]){
-//             agregated[j*shift] += arrays[i][j];
-//             console.log('blob');
-//         }else{
-//             agregated.push(arrays[i][j]);
-//         }
-//       }
-//     }
 
-//     return agregated;
-// }
 
 function addingShifted (arrays, shift) {
     let result = [];
