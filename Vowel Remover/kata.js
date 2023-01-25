0bf1bbfae37c647f2c85b8a1523905a9902e92a7
Gr34sy <slupek3@poczta.fm>
2023-01-25 18:40:19 +0100

    added Pig Latin task

---
+++ Vowel Remover/kata.js
@@ -0,0 +1,23 @@
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+//don't worry about uppercase vowels
+//y is not considered a vowel for this kata
+
+
+// 1 
+function shortcut(string){
+    console.log(
+        string.replace(/[a,e,i,o,u]/g,'')
+    );
+}
+
+//2 oneliner 
+const  shortcutO = (str) => str.replace(/[aeiou]/g,'');
+
+
+//3 oneliner which works both on lowercase and uppercase characters
+const  shortcutOB = (str) => str.replace(/[aeiou]/gi,'');
+
+// the "/[aeiou]/"  is regExp (regular expression) and "gi" are modifiers
+// g - global
+// i - insensitive (means both upper and lowercase)
